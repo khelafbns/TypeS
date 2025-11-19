dadc21e1034a985434b7eb27c8b597548c3ea9a2
khelafbns <kh.8bns@gmail.com>
2025-11-19 20:36:24 +0100

    prjct ts

--- dist/Corse.js
+++ dist/Corse.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let age = 20;
+if (age < 15)
+    age += 10;
+console.log(age);
 //# sourceMappingURL=Corse.js.map
--- src/Corse.ts
+++ src/Corse.ts
@@ -1,3 +1,4 @@
 let age: number = 20;
 if(age<15)
     age+=10;
+console.log(age);
