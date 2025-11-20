a204fcbf24b95e45ce3a5d16369a9368ff886798
khelafbns <kh.8bns@gmail.com>
2025-11-20 14:14:00 +0100

    Functions

--- src/Corse.ts
+++ src/Corse.ts
@@ -1,6 +1,6 @@
 let age: number = 20;
-if(age<25)
-    age+=10;
+if (age < 25)
+    age += 10;
 console.log(age);
 //Types
 
@@ -8,29 +8,37 @@ let sales: number = 123_456_789;
 let cousre: string = "TSx";
 let isPublished: boolean = true;
 
-let  level:any;
+let level: any;
 level = 1;
 level = "a";
 
-function render(document:any){
+function render(document: any) {
     console.log(document)
 }
 
 //Arrays
-let  words : string[] = ["1","2","3"];
-let  numbers : number[] = [1,2,3];
-let  bools : boolean[] = [true,false,true];
-let  arr : any[] = [1,true,"a"];
-let Numbers :Array<number> = [1,2,3];
+let words: string[] = ["1", "2", "3"];
+let numbers: number[] = [1, 2, 3];
+let bools: boolean[] = [true, false, true];
+let arr: any[] = [1, true, "a"];
+let Numbers: Array<number> = [1, 2, 3];
 
 //Tuples
-let user: [number,string] = [1,"a"];
+let user: [number, string] = [1, "a"];
 
 //Enums
 // const small = 1;
 // const medium = 2;
 // const large = 3;
 //PascalCase
-enum Size {Small=1, Medium, Large}
-let mySize: Size= Size.Medium;
+enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
 console.log(mySize);
+
+//Functions
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.4;
+}
+calculateTax(10_000);
