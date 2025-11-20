2dd119e5b80b263a758d304dfe9b2f6a119b7982
khelafbns <kh.8bns@gmail.com>
2025-11-20 14:48:18 +0100

    Optional Chaining

--- src/Corse.ts
+++ src/Corse.ts
@@ -96,11 +96,23 @@ let quantity: Quantity = 100;
 type Metric = "cm" | "inch";
 
 //Nullable Types
-function greet(name: string | null ) {
-    if(name){
+function greet(name: string | null) {
+    if (name) {
         console.log(name.toUpperCase());
-    }else{
+    } else {
         console.log("Hola!");
     }
 }
 greet(null);
+
+//Optional Chaining
+type Customer = {
+    birthday?: Date
+}
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear());
+
+//Optional Chaining with Arrays
