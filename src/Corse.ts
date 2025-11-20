ebaa5fe00bf48657b5fde7106cd3fee2c3a2e522
khelafbns <kh.8bns@gmail.com>
2025-11-20 13:58:22 +0100

    Arrays

--- src/Corse.ts
+++ src/Corse.ts
@@ -2,6 +2,7 @@ let age: number = 20;
 if(age<25)
     age+=10;
 console.log(age);
+//Types
 
 let sales: number = 123_456_789;
 let cousre: string = "TSx";
@@ -14,3 +15,10 @@ level = "a";
 function render(document:any){
     console.log(document)
 }
+
+//Arrays
+let  words : string[] = ["1","2","3"];
+let  numbers : number[] = [1,2,3];
+let  bools : boolean[] = [true,false,true];
+let  arr : any[] = [1,true,"a"];
+let Numbers :Array<number> = [1,2,3];
