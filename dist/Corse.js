ac53a21c9e205a2a623427d584fa54c1cc7eed54
khelafbns <kh.8bns@gmail.com>
2025-11-20 13:53:54 +0100

    Built-In Type

--- dist/Corse.js
+++ dist/Corse.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let age = 20;
-if (age < 15)
+if (age < 25)
     age += 10;
 console.log(age);
 //# sourceMappingURL=Corse.js.map
--- src/Corse.ts
+++ src/Corse.ts
@@ -1,4 +1,16 @@
 let age: number = 20;
-if(age<15)
+if(age<25)
     age+=10;
 console.log(age);
+
+let sales: number = 123_456_789;
+let cousre: string = "TSx";
+let isPublished: boolean = true;
+
+let  level:any;
+level = 1;
+level = "a";
+
+function render(document:any){
+    console.log(document)
+}
