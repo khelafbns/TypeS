71b695f3faa39d5b53f5b8988203bddc4d8af52a
khelafbns <kh.8bns@gmail.com>
2025-11-20 14:35:29 +0100

    Literal Types

--- src/Corse.ts
+++ src/Corse.ts
@@ -88,3 +88,9 @@ let textBox: UIWidget = {
     drag: () => { },
     resize: () => { }
 }
+
+//Literal Types
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = "cm" | "inch";
